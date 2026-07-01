134c7ac92a4fd5cf1b5f3996f397ca8691b03f72
Larsimoto007 <lars.matthiesen@ias-software.de>
2026-07-01 13:27:41 +0200

    fix: move bare-mode init to inline script in body - runs synchronously before bundle.js

--- src/app/index.ts
+++ src/app/index.ts
@@ -6,25 +6,6 @@ import { Tool } from './client/Tool';
 window.onload = async function (): Promise<void> {
     const hash = location.hash.replace(/^#!/, '');
     const parsedQuery = new URLSearchParams(hash);
-    if (parsedQuery.get('bare') === '1') {
-        document.body.classList.add('bare-mode');
-        document.title = '';
-        var toolbarSel = '.control-buttons-list,.control-button,.control-wrapper,.more-box,.toolbox,.device-list,nav,header,footer';
-        var observer = new MutationObserver(function (mutations) {
-            for (var mi = 0; mi < mutations.length; mi++) {
-                var added = mutations[mi].addedNodes;
-                for (var ni = 0; ni < added.length; ni++) {
-                    var node = added[ni];
-                    if (node instanceof HTMLElement) {
-                        if (node.matches(toolbarSel)) node.remove();
-                        var list = node.querySelectorAll(toolbarSel);
-                        for (var li = 0; li < list.length; li++) list[li].remove();
-                    }
-                }
-            }
-        });
-        observer.observe(document.body, { childList: true, subtree: true });
-    }
     const action = parsedQuery.get('action');
 
     /// #if USE_BROADWAY
